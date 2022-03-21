e26daba4f5627a0294e4fba4b37986045465196e
Fumichannel <98387627+Fumichannel@users.noreply.github.com>
2022-03-21 20:42:20 +0900

    Add files via upload

--- sample.js
+++ sample.js
@@ -4,19 +4,18 @@ function opens(texts){
 
 
 function header(){
-    var aaaa = '' + 
-'	<style>.menu div{display: inline-block; padding: 30px; border-left: solid 1px #fff;color: #fff;}</style>	   '+
-'	<div style="  width: 100%; position: fixed; display: flex; background-color: rgb(0, 150, 135);">	   '+
-'		   '+
-'	<span  onclick="location.href = "https://fumichannel.github.io/"" style=" width: 300px; top: 0; font-family: 英語筆記体フォント; color: #fff; font-size: 45px;">Fumi-channel</span>	   '+
-'	<div class="menu" style=" position: absolute; height: 20px; display: inline-block; right: 0;">	   '+
-'	<div onclick="location.href = "https://fumichannel.github.io/"">ホーム</div>	   '+
-'	<div>配布物</div>	   '+
-'	<div onclick="opens("https://discord.gg/hx2Avaa4kH")">discord</div>	   '+
-'	<div>作り方講座</div>	   '+
-'	<div>そのほか</div>	   '+
-'	</div>	   '+
-'	</div>	   '
+    var aaaa =    '	<style>menu div{display: inline-block; padding: 30px; border-left: solid 1px #fff;color: #fff;}.menu a{display: inline-block; padding: 30px; border-left: solid 1px #fff;color: #fff;text-decoration:none;}</style>	   '+
+    '	<div style="  width: 100%; position: fixed; display: flex; background-color: rgb(0, 150, 135);">	   '+
+    '		   '+
+    '	<a href="https://fumichannel.github.io/" style=" width: 300px; top: 0; font-family: 英語筆記体フォント;color: #fff;text-decoration:none; font-size: 45px;">Fumi-channel</a>	   '+
+    '	<div class="menu" style=" position: absolute; height: 20px; display: inline-block; right: 0;">	   '+
+    '	<a  onclick="location.href = "https://fumichannel.github.io/">ホーム</a>	   '+
+    '	<a  target="_blank">配布物</a>	   '+
+    '	<a href="https://discord.gg/hx2Avaa4kH" target="_blank">discord</a>	   '+
+    '	<a  target="_blank">作り方講座</a>	   '+
+    '	<a  target="_blank">そのほか</a>	   '+
+    '	</div>	   '+
+    '	</div>	   '
 
 document.getElementById('mydiv').innerHTML = aaaa
 }
