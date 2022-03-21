93c368afc516fbf7588e09ea116a78d4a8eb1909
Fumichannel <98387627+Fumichannel@users.noreply.github.com>
2022-03-21 20:45:45 +0900

    Add files via upload

--- sample.js
+++ sample.js
@@ -9,7 +9,7 @@ function header(){
     '		   '+
     '	<a href="https://fumichannel.github.io/" style=" width: 300px; top: 0; font-family: 英語筆記体フォント;color: #fff;text-decoration:none; font-size: 45px;">Fumi-channel</a>	   '+
     '	<div class="menu" style=" position: absolute; height: 20px; display: inline-block; right: 0;">	   '+
-    '	<a  onclick="location.href = "https://fumichannel.github.io/">ホーム</a>	   '+
+    '	<a href="https://fumichannel.github.io/">ホーム</a>	   '+
     '	<a  target="_blank">配布物</a>	   '+
     '	<a href="https://discord.gg/hx2Avaa4kH" target="_blank">discord</a>	   '+
     '	<a  target="_blank">作り方講座</a>	   '+
